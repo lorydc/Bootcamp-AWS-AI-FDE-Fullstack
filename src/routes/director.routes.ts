ffe705a6d39283243887f246c7e31369baad01f9
Lorena <lorenasantost12@gmail.com>
2026-04-24 12:50:56 -0300

    fix: add validation middleware to director update route

--- src/routes/director.routes.ts
+++ src/routes/director.routes.ts
@@ -12,9 +12,9 @@ import { validateDirector } from "../middlewares/validateDirector";
 const router = Router();
 
 router.post("/directors", validateDirector, createDirectorController);
+router.put("/directors/:id", validateDirector, updateDirectorController);
 router.get("/directors", getAllDirectorsController);
 router.get("/directors/:id", getDirectorByIdController);
 router.delete("/directors/:id", deleteDirectorController);
 router.get("/directors/:id/movies", getMoviesByDirectorController);
-router.put("/directors/:id", updateDirectorController);
 export default router;
